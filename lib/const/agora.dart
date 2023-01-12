27d6d4e441f0d72b3fe210d8ef9b57b0e4a87bc1
zoaseo <101240239+zoaseo@users.noreply.github.com>
2023-01-12 13:07:25 +0900

    upload

--- lib/const/agora.dart
+++ lib/const/agora.dart
@@ -1,5 +1,5 @@
-const APP_ID = "26aefa862c904499a336e4ff04995b2a";
+const APP_ID = "APP_ID";
 
 const CHANNEL_NAME = "testchannel";
 
-const TEMP_TOKEN = "007eJxTYIhh6zt3+bmBHFtWcPp13gU22xxDfRtWnmkIFmzasyPiUJUCQ6qZmaWZUVpaqmFaiomZkamlZaKZRZpxorGFmaW5qYmJdsa+5IZARgYuplJWRgYIBPG5GUpSi0uSMxLz8lJzGBgAo7ofyw==";
+const TEMP_TOKEN = "TEMP_TOKEN";
